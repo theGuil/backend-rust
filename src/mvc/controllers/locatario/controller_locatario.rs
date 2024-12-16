3687ddb67454cc7918445088521b7588de6b39ce
theGuil <seflanguilhermesouza@gmail.com>
2024-12-16 09:10:46 -0300

    :sparkles: feat: Adiciona identação no código

--- src/mvc/controllers/locatario/controller_locatario.rs
+++ src/mvc/controllers/locatario/controller_locatario.rs
@@ -6,11 +6,11 @@ pub mod controller_locatario {
     use crate::mvc::models::locatario::model_locatario::model_locatario;
 
     pub async fn cadastrar_locatario()  {
-            let id: i32 = 150000;
-            let codigo: String = "LOC-2323213-2024".to_string();
-            let cnpjcpf: String = "10548372950".to_string();
-            let nome: String = "Guilherme de Souza".to_string();
-            model_locatario::Locatario::new(id, codigo, cnpjcpf, nome);
+        let id: i32 = 150000;
+        let codigo: String = "LOC-2323213-2024".to_string();
+        let cnpjcpf: String = "10548372950".to_string();
+        let nome: String = "Guilherme de Souza".to_string();
+        model_locatario::Locatario::new(id, codigo, cnpjcpf, nome);
     }
 
     pub async  fn buscar_locatario() -> impl IntoResponse {
@@ -19,6 +19,6 @@ pub mod controller_locatario {
 
     pub async  fn buscar_locatario_documento(Path(documento): Path<String>) -> impl IntoResponse {
         print!("documento, ${}",documento);
-          model_locatario::Locatario::buscar_por_documento(&documento).await
+        model_locatario::Locatario::buscar_por_documento(&documento).await
     }
 }
